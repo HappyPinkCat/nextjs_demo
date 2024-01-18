4a9d6770ed21dd293777daad0452be340bb4416b
lpgong <lpgong@trip.com>
2024-01-18 15:42:00 +0800

    feat: fetch data&ts

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,28 @@
 // 对应链接：http://localhost:3000/users
 import React from "react";
 
-const page = () => {
-  return <div>123</div>;
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+const page = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default page;
