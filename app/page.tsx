5f1f31e8142e2c70fcb81fb205e787c9509dce8f
lpgong <lpgong@trip.com>
2024-01-18 15:00:28 +0800

    feat: client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // 对应链接：http://localhost:3000
 import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>hello darling~</h1>
       <Link href="/users">users</Link>
+      <ProductCard />
     </main>
   );
 }
