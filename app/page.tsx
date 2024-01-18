331b822b7526994ca21d8944682d4ab0a7f59d26
lpgong <lpgong@trip.com>
2024-01-18 14:13:13 +0800

    feat: navigation-Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // 对应链接：http://localhost:3000
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>hello darling~</h1>
+      <Link href="/users">users</Link>
     </main>
   );
 }
