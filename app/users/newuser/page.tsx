96580a726f526f9ec27ee0ee82c6ed6bf8868b60
lpgong <lpgong@trip.com>
2024-01-18 12:54:03 +0800

    feat: init project&router

---
+++ app/users/newuser/page.tsx
@@ -0,0 +1,8 @@
+// 对应链接：http://localhost:3000/users/newuser
+import React from "react";
+
+const page = () => {
+  return <div>newuser</div>;
+};
+
+export default page;
---
+++ app/users/page.tsx
@@ -0,0 +1,8 @@
+// 对应链接：http://localhost:3000/users
+import React from "react";
+
+const page = () => {
+  return <div>123</div>;
+};
+
+export default page;
